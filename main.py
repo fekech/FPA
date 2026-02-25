afa45f7ceaa847b98e76f082431a2fccb16cb63c
fekech <filyakert@gmail.com>
2026-02-25 22:07:28 +0300

    Добавил Kivy + requ.txt

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sys , os
+import sys , os , configparser
 
 # Добавление родительской директории к sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Импортирование модулей приложения
-from FPA.Data.Scripts.cmd import CMD_AuthorLogo
+from Data.Scripts.cmd import CMD_AuthorLogo
 
 CMD_AuthorLogo()
